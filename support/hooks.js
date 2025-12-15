694622d8cafd4a7efd7a436b0b8380360dfcaad5
mirza shakeeb <shakeeb717171@gmail.com>
2025-12-15 14:26:56 +0530

    intentionally failed

--- support/hooks.js
+++ support/hooks.js
@@ -1,19 +1,31 @@
 const { Before, After, AfterStep } = require("@cucumber/cucumber");
 const fs = require("fs");
+const path = require("path");
 
 Before(async function () {
   await this.openBrowser();
-  this.page = await this.context.newPage(); 
+  this.page = await this.context.newPage();
 });
 
-AfterStep(async function ({ result }) {
-  if (result.status === "failed") {
-    if (!fs.existsSync("./reports/screenshots")) {
-      fs.mkdirSync("./reports/screenshots", { recursive: true });
+AfterStep(async function ({ result, pickle }) {
+  if (result.status === "FAILED") {
+    const dir = path.join("reports", "screenshots");
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
-    await this.page.screenshot({
-      path: `./reports/screenshots/${Date.now()}.png`
+
+    const fileName = `${pickle.name.replace(/[^a-zA-Z0-9]/g, "_")}.png`;
+    const filePath = path.join(dir, fileName);
+
+    // Take screenshot as buffer
+    const screenshot = await this.page.screenshot({
+      path: filePath,
+      fullPage: true,
     });
+
+    //  Attach screenshot to Cucumber report
+    await this.attach(screenshot, "image/png");
   }
 });
 
