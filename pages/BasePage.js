6def9a229accbf1288f39fea1067be7fd0f16b98
mirza shakeeb <shakeeb717171@gmail.com>
2025-12-15 14:49:16 +0530

    final test

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -3,9 +3,15 @@ class BasePage {
     this.page = page;
   }
 
-  async navigate(url) {
-    await this.page.goto(url);
-  }
+async navigate(url) {
+  await this.page.goto(url, {
+    waitUntil: "domcontentloaded",
+    timeout: 60000
+  });
+
+  // Extra stabilization for WebKit
+  await this.page.waitForLoadState("networkidle", { timeout: 60000 }).catch(() => {});
+}
 
   async click(selector) {
     await this.page.click(selector);
